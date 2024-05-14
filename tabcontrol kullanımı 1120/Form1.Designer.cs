386e8bd2538d2a9ed864ae374ce596a2639a1b4e
rabiaakkus <145451092+rabiaakkus@users.noreply.github.com>
2024-05-14 14:15:00 +0300

    Add files via upload

---
+++ tabcontrol kullanımı 1120/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace tabcontrol_kullanımı_1120
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dgwFilmler = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblFilmAd = new System.Windows.Forms.Label();
+            this.lblSure = new System.Windows.Forms.Label();
+            this.lblYil = new System.Windows.Forms.Label();
+            this.lblYonetmen = new System.Windows.Forms.Label();
+            this.lblTur = new System.Windows.Forms.Label();
+            this.lblOdul = new System.Windows.Forms.Label();
+            this.lblPuan = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwFilmler)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 22);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(370, 348);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dgwFilmler);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(362, 322);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Listele";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dgwFilmler
+            // 
+            this.dgwFilmler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwFilmler.Location = new System.Drawing.Point(35, 152);
+            this.dgwFilmler.Name = "dgwFilmler";
+            this.dgwFilmler.Size = new System.Drawing.Size(240, 150);
+            this.dgwFilmler.TabIndex = 0;
+            this.dgwFilmler.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwFilmler_CellContentClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(362, 322);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Güncelle";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(362, 322);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Ekle";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(392, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "film_ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(392, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "yonetmen ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(392, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "yil";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(392, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "tur";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(392, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 18);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "sure";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(392, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 18);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "IMDB_puan";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(390, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 18);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Ödül";
+            // 
+            // lblFilmAd
+            // 
+            this.lblFilmAd.AutoSize = true;
+            this.lblFilmAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblFilmAd.Location = new System.Drawing.Point(478, 44);
+            this.lblFilmAd.Name = "lblFilmAd";
+            this.lblFilmAd.Size = new System.Drawing.Size(176, 18);
+            this.lblFilmAd.TabIndex = 8;
+            this.lblFilmAd.Text = "_____________________";
+            // 
+            // lblSure
+            // 
+            this.lblSure.AutoSize = true;
+            this.lblSure.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSure.Location = new System.Drawing.Point(478, 120);
+            this.lblSure.Name = "lblSure";
+            this.lblSure.Size = new System.Drawing.Size(176, 18);
+            this.lblSure.TabIndex = 9;
+            this.lblSure.Text = "_____________________";
+            // 
+            // lblYil
+            // 
+            this.lblYil.AutoSize = true;
+            this.lblYil.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYil.Location = new System.Drawing.Point(478, 96);
+            this.lblYil.Name = "lblYil";
+            this.lblYil.Size = new System.Drawing.Size(176, 18);
+            this.lblYil.TabIndex = 10;
+            this.lblYil.Text = "_____________________";
+            // 
+            // lblYonetmen
+            // 
+            this.lblYonetmen.AutoSize = true;
+            this.lblYonetmen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYonetmen.Location = new System.Drawing.Point(478, 69);
+            this.lblYonetmen.Name = "lblYonetmen";
+            this.lblYonetmen.Size = new System.Drawing.Size(184, 18);
+            this.lblYonetmen.TabIndex = 11;
+            this.lblYonetmen.Text = "______________________";
+            // 
+            // lblTur
+            // 
+            this.lblTur.AutoSize = true;
+            this.lblTur.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTur.Location = new System.Drawing.Point(478, 172);
+            this.lblTur.Name = "lblTur";
+            this.lblTur.Size = new System.Drawing.Size(176, 18);
+            this.lblTur.TabIndex = 12;
+            this.lblTur.Text = "_____________________";
+            // 
+            // lblOdul
+            // 
+            this.lblOdul.AutoSize = true;
+            this.lblOdul.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOdul.Location = new System.Drawing.Point(478, 205);
+            this.lblOdul.Name = "lblOdul";
+            this.lblOdul.Size = new System.Drawing.Size(176, 18);
+            this.lblOdul.TabIndex = 13;
+            this.lblOdul.Text = "_____________________";
+            // 
+            // lblPuan
+            // 
+            this.lblPuan.AutoSize = true;
+            this.lblPuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPuan.Location = new System.Drawing.Point(478, 145);
+            this.lblPuan.Name = "lblPuan";
+            this.lblPuan.Size = new System.Drawing.Size(176, 18);
+            this.lblPuan.TabIndex = 14;
+            this.lblPuan.Text = "_____________________";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(410, 271);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 172);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label15.Location = new System.Drawing.Point(407, 250);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(50, 18);
+            this.label15.TabIndex = 16;
+            this.label15.Text = "poster";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(720, 455);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblPuan);
+            this.Controls.Add(this.lblOdul);
+            this.Controls.Add(this.lblTur);
+            this.Controls.Add(this.lblYonetmen);
+            this.Controls.Add(this.lblYil);
+            this.Controls.Add(this.lblSure);
+            this.Controls.Add(this.lblFilmAd);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwFilmler)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblFilmAd;
+        private System.Windows.Forms.Label lblSure;
+        private System.Windows.Forms.Label lblYil;
+        private System.Windows.Forms.Label lblYonetmen;
+        private System.Windows.Forms.Label lblTur;
+        private System.Windows.Forms.Label lblOdul;
+        private System.Windows.Forms.Label lblPuan;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.DataGridView dgwFilmler;
+    }
+}
+
---
+++ tabcontrol kullanımı 1120/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace tabcontrol_kullanımı_1120
+{
+    public partial class Form1 : Form
+    {
+        string baglanti = "Server=localhost;Database=hnk_okul;Uid=root;Pwd=;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DgwDoldur();
+        }
+        void DgwDoldur()
+        {
+            using (MySqlConnection baglan = new MySqlConnection(baglanti))
+            {
+                baglan.Open();
+                string sorgu = "SELECT * FROM filmler;";
+
+                MySqlCommand cmd = new MySqlCommand(sorgu, baglan);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+             
+                dgwFilmler.DataSource = dt;
+                dgwFilmler.Columns["film_odul"].Visible = false;
+                dgwFilmler.Columns["yonetmen"].Visible = false;
+                dgwFilmler.Columns["poster"].Visible = false;
+                dgwFilmler.Columns["sure"].Visible = false;
+                dgwFilmler.Columns["film_id"].Visible = false;
+
+
+            }
+        }
+
+        private void dgwFilmler_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgwFilmler.SelectedRows.Count > 0)
+            {
+                lblFilmAd.Text = dgwFilmler.SelectedRows[0].Cells["film_ad"].Value.ToString();
+                lblYonetmen.Text = dgwFilmler.SelectedRows[0].Cells["yonetmen"].Value.ToString();
+                lblPuan.Text = dgwFilmler.SelectedRows[0].Cells["imdb_puan"].Value.ToString();
+                lblSure.Text = dgwFilmler.SelectedRows[0].Cells["sure"].Value.ToString();
+                lblYil.Text = dgwFilmler.SelectedRows[0].Cells["yil"].Value.ToString();
+                lblTur.Text = dgwFilmler.SelectedRows[0].Cells["tur"].Value.ToString();
+
+
+                lblOdul.Text = "ödül yok";
+                if (Convert.ToBoolean(dgwFilmler.SelectedRows[0].Cells["odul"].Value))
+                {
+                    lblOdul.Text = "ödüllü film";
+                }
+
+
+                string resimyol = Path.Combine(Environment.CurrentDirectory, "poster", dgwFilmler.SelectedRows[0].Cells["poster"].Value.ToString());
+
+                if (File.Exists(resimyol))
+                {
+                    pictureBox1.ImageLocation = resimyol;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+
+
+
+
+            }
+        }
+    }
+}
